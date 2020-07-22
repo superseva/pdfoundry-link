e5ec12f424cc05a523c4cccf0c9f585d919cdf7b
superseva <superseva@gmail.com>
2020-07-22 10:49:35 +0200

    added scripts

---
+++ pdfoundry-link.js
@@ -0,0 +1,14 @@
+Hooks.on("renderItemSheet", (app, html, options) => {
+            searchPdfLinks(html);
+});
+
+Hooks.on("renderJournalSheet", (app, html, options) => {
+            searchPdfLinks(html);
+});
+function searchPdfLinks (html){
+    html.find('.pdfoundry_link').click((ev) => {
+                    let element = ev.currentTarget;
+                    console.log (element.dataset);
+					ui.PDFoundry.openPDFByName(element.dataset.pdf, parseInt(element.dataset.page));					
+            });
+}
